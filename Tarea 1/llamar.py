56dcecfe87ccecad002522465d472fde422b228b
conti89 <contigomez89@gmail.com>
2012-09-03 07:28:09 -0400

    llamar listo

--- Tarea 1/llamar.py
+++ Tarea 1/llamar.py
@@ -1,16 +1,31 @@
 from process import Process
+from historial import Historial
+import datetime
 
 class Llamar(Process):
-	def __init__(self,name,type,priority,numero,tejec):
-		Process.init(self,name,type,priority)
+	def __init__(self,name,type,priority,numero):
+		Process.__init__(self,name,type,priority)
 		self.numero = numero;
-		self.tejec = tejec;
+		self.hist = Historial()
 	def getNumero(self):
 		return self.numero;
-	def getTEjec(self):
-		return self.tejec;
-	def historial():
-		archivo = open("historial.txt","w");
+	def setTejec(self, duracion):
+		self.duracion = duracion
+	def runTimer(self):
+		self.tini = datetime.datetime.now()
+		print "Llamando a ...", self.numero
+		self.fin = raw_input("Para finalizar presione 1: ")
+		if(self.fin=='1'):
+			self.tfin=datetime.datetime.now()
+			self.duracion = self.tfin-self.tini
+			print "Llamada finalizada.  Duracion: ", self.duracion
+			self.hist.Actualizar('llamada',self.numero, self.tini)
+
+			
+		
+		
+		
+	
 		
 		
 	
